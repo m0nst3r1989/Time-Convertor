0e5f178ad84b8cb2e677e86c71d4340a22d540ca
m0nst3r1989 <113396501+m0nst3r1989@users.noreply.github.com>
2022-09-24 19:03:26 -0400

    Add files via upload

---
+++ Time Convertor/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Time_Convertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.timeConverted = new System.Windows.Forms.TextBox();
+            this.textBoxSeconds = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.convertor_Click);
+            // 
+            // timeConverted
+            // 
+            this.timeConverted.Location = new System.Drawing.Point(458, 136);
+            this.timeConverted.Name = "timeConverted";
+            this.timeConverted.Size = new System.Drawing.Size(100, 22);
+            this.timeConverted.TabIndex = 1;
+            // 
+            // textBoxSeconds
+            // 
+            this.textBoxSeconds.Location = new System.Drawing.Point(458, 49);
+            this.textBoxSeconds.Name = "textBoxSeconds";
+            this.textBoxSeconds.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSeconds.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seconds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(312, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time Converted";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSeconds);
+            this.Controls.Add(this.timeConverted);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Time Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox timeConverted;
+        private System.Windows.Forms.TextBox textBoxSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Time Convertor/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time_Convertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertor_Click(object sender, EventArgs e)
+        {
+                int seconds = Convert.ToInt32(textBoxSeconds.Text);
+
+                if (seconds >= 86400)
+                {
+                    int days = seconds / 86400;
+                    timeConverted.Text = days + " days";
+                }
+                else if (seconds >= 3600)
+                {
+                    int hours = seconds / 3600;
+                    timeConverted.Text = hours + " hours";
+                }
+                else if (seconds >= 60)
+                {
+                    int minutes = seconds / 60;
+                    timeConverted.Text = minutes + " minutes";
+                }
+                else
+                {
+                    timeConverted.Text = seconds + " seconds";
+                }
+            }
+
+
+        }
+
+  
+}
